5c8338cd27cee334fd6d7c0088c8eaff4acde672
weihan <weihanlai.tw@gmail.com>
2025-12-28 13:19:01 +0800

    update: dashboard input list

--- static/dashboard/main.js
+++ static/dashboard/main.js
@@ -2,9 +2,24 @@ import { logout } from "../auth.js";
 import { requireLogin } from "../auth.js";
 import { showLoading, hideLoading } from "../auth.js";
 
+/*
 showLoading();
 await requireLogin();
-hideLoading();
+hideLoading();*/
+
+const university_input = document.getElementById("university_input");
+const university_list = document.getElementById("university_list");
+
+function isValid(val){
+    return Array.from(university_list.options).some(o => o.value === val);
+}
+
+university_input.addEventListener("blur", () => {
+    if (university_input.value && !isValid(university_input.value.trim())) {
+        alert("請從清單選擇");
+        university_input.value = "";
+    }
+});
 
 const logoutBtn = document.getElementById("logout_btn");
 if (!logoutBtn) {
